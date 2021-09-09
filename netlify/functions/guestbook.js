4ff8d91f18e2c095d7868b1d0b6b23361c3b3535
himynameisdave <dave@fossa.com>
2021-09-09 13:31:51 -0400

    test

--- netlify/functions/guestbook.js
+++ netlify/functions/guestbook.js
@@ -20,6 +20,7 @@ exports.handler = async function(event, context) {
     await supabase.from('guestbook').insert([
       { name, message }
     ]);
+
     return {
       statusCode: 201,
       body: JSON.stringify({
